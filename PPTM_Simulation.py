9a4136e1abe1fa5c5f9657211ad129fd8d0148ae
Daniel Stilman <daniel.stilman@gmail.com>
2023-09-14 16:05:14 +0300

    Refactored to reflect the terminology used in the proof I'm constructing.

--- PPTM_Simulation.py
+++ PPTM_Simulation.py
@@ -1,12 +1,12 @@
 import random
 import itertools
 
-# Deterministic Turing Machine 1 (DTM1)
+# Optimal Halting Decider 1 (OHD1)
 # This machine tries to decide whether a given function halts.
-# However, if the function is specifically adversarial towards DTM1, 
+# However, if the function is specifically pathological towards OHD1, 
 # the machine gives up and says "Cannot decide".
-def deterministic_decider_1(f):
-    if f.choices[-1] == 'adversary_decider_1':
+def OHD1(f):
+    if f.choices[-1] == 'pathological_decider_1':
         return "Cannot decide"
     
     result = f()
@@ -15,10 +15,10 @@ def deterministic_decider_1(f):
     elif result == "Doesn't halt":
         return "Doesn't halt"
 
-# Deterministic Turing Machine 2 (DTM2)
-# This machine operates similarly to DTM1, but it's sensitive to a different adversarial function.
-def deterministic_decider_2(f):
-    if f.choices[-1] == 'adversary_decider_2':
+# Optimal Halting Decider 2 (OHD2)
+# This machine operates similarly to OHD1, but it's sensitive to a different pathological function.
+def OHD2(f):
+    if f.choices[-1] == 'pathological_decider_2':
         return "Cannot decide"
     
     result = f()
@@ -28,49 +28,49 @@ def deterministic_decider_2(f):
         return "Doesn't halt"
 
 # Partially Probabilistic Turing Machine (PPTM)
-# This machine randomly chooses either DTM1 or DTM2 to decide if a function halts.
-# If the chosen DTM cannot decide, the PPTM continues the loop until a decision is made.
-# This is important as it effectively "masks" the PPTM's decisions from any DTM, as they cannot emulate it.
+# This machine randomly chooses either OHD1 or OHD2 to decide if a function halts.
+# If the chosen OHD cannot decide, the PPTM continues the loop until a decision is made.
+# This is important as it effectively "masks" the PPTM's decisions from any OHD, as they cannot emulate it.
 def PPTM(f):
     while True:
-        roll = random.choice([deterministic_decider_1, deterministic_decider_2])
+        roll = random.choice([OHD1, OHD2])
         result = roll(f)
         
         if result != "Cannot decide":
             return result
 
-# This function creates a set of all possible adversary configurations up to a given length.
-# This models the general concept of how a DTM can model any NDTM, but as we will see, in adversarial conditions this is not the case
-def adversary_simulations(x):
-    adversaries = ['adversary_decider_1', 'adversary_decider_2']
+# This function creates a set of all possible pathological configurations up to a given length.
+# This models the general concept of how a DTM can model any NDTM, but as we will see, in pathological conditions this is not the case
+def pathological_simulations(x):
+    pathologies = ['pathological_decider_1', 'pathological_decider_2']
     for length in range(1, x+1):
-        for choices in itertools.product(adversaries, repeat=length):
+        for choices in itertools.product(pathologies, repeat=length):
             yield list(choices)
 
-# This represents the adversarial function. 
-# It uses the simulation list to decide which "emulation" of the PPTM to use
+# This represents the pathological function. 
+# It uses the simulation list to decide which "emulation" of the PPTM to use.
 # It can't actually "pick" non-deterministically, so it ultimately has to resort
 # To deterministically choosing a particular set of choices the PPTM might make.
 # However, because the PPTM specifically countermands it, this strategy can never succeed.
-def adversary_decider():
-    choices = adversary_decider.choices  # Set externally before calling
+def pathological_decider():
+    choices = pathological_decider.choices  # Set externally before calling
     target = choices[-1]
     
-    if target == 'adversary_decider_1':
+    if target == 'pathological_decider_1':
         return "Halts"
-    elif target == 'adversary_decider_2':
+    elif target == 'pathological_decider_2':
         return "Doesn't halt"
 
-# Here, we test the PPTM against the range of adversary configurations.
-# We keep count of how many times the PPTM correctly determines the adversary's decision.
+# Here, we test the PPTM against the range of pathological configurations.
+# We keep count of how many times the PPTM correctly determines the pathological's decision.
 max_length = 5
 correct = 0
 total = 0
 
-for choices in adversary_simulations(max_length):
-    adversary_decider.choices = choices
-    result = PPTM(adversary_decider)
-    expected = adversary_decider()
+for choices in pathological_simulations(max_length):
+    pathological_decider.choices = choices
+    result = PPTM(pathological_decider)
+    expected = pathological_decider()
     
     if result == expected:
         correct += 1
