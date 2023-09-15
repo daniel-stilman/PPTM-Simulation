b4474823f53f109d19022d608f603d66ad4d83d8
Daniel Stilman <daniel.stilman@gmail.com>
2023-09-15 23:05:20 +0300

    Updated terminology to reflect current state of the proof

--- PPTM_Simulation.py
+++ PPTM_Simulation.py
@@ -1,12 +1,22 @@
 import random
 import itertools
 
-# Optimal Halting Decider 1 (OHD1)
+# MBPD Judge 1
+# This function tries to decide if the function f is specifically pathological towards MBPD1.
+def MBPD_judge1(f):
+    return f.choices[-1] == 'pathological_decider_1'
+
+# MBPD Judge 2
+# Similar to MBPD_judge1, but for MBPD2.
+def MBPD_judge2(f):
+    return f.choices[-1] == 'pathological_decider_2'
+
+# MBPD1 (Maximally Broad Propositional Decider 1)
 # This machine tries to decide whether a given function halts.
-# However, if the function is specifically pathological towards OHD1, 
-# the machine gives up and says "Cannot decide".
-def OHD1(f):
-    if f.choices[-1] == 'pathological_decider_1':
+# If the function is specifically pathological towards MBPD1,
+# it gives up and says "Cannot decide".
+def MBPD1(f):
+    if MBPD_judge1(f):
         return "Cannot decide"
     
     result = f()
@@ -15,10 +25,10 @@ def OHD1(f):
     elif result == "Doesn't halt":
         return "Doesn't halt"
 
-# Optimal Halting Decider 2 (OHD2)
-# This machine operates similarly to OHD1, but it's sensitive to a different pathological function.
-def OHD2(f):
-    if f.choices[-1] == 'pathological_decider_2':
+# MBPD2 (Maximally Broad Propositional Decider 2)
+# Operates similarly to MBPD1, but is sensitive to a different pathological function.
+def MBPD2(f):
+    if MBPD_judge2(f):
         return "Cannot decide"
     
     result = f()
@@ -27,33 +37,28 @@ def OHD2(f):
     elif result == "Doesn't halt":
         return "Doesn't halt"
 
-# Partially Probabilistic Turing Machine (PPTM)
-# This machine randomly chooses either OHD1 or OHD2 to decide if a function halts.
-# If the chosen OHD cannot decide, the PPTM continues the loop until a decision is made.
-# This is important as it effectively "masks" the PPTM's decisions from any OHD, as they cannot emulate it.
+# PPTM (Partially Probabilistic Turing Machine)
+# Randomly chooses either MBPD1 or MBPD2 to decide if a function halts.
+# If the chosen MBPD cannot decide, the PPTM continues the loop until a decision is made.
 def PPTM(f):
     while True:
-        roll = random.choice([OHD1, OHD2])
+        roll = random.choice([MBPD1, MBPD2])
         result = roll(f)
         
         if result != "Cannot decide":
             return result
 
-# This function creates a set of all possible pathological configurations up to a given length.
-# This models the general concept of how a DTM can model any NDTM, but as we will see, in pathological conditions this is not the case
+# Generates all possible pathological configurations up to a given length.
 def pathological_simulations(x):
     pathologies = ['pathological_decider_1', 'pathological_decider_2']
     for length in range(1, x+1):
         for choices in itertools.product(pathologies, repeat=length):
             yield list(choices)
 
-# This represents the pathological function. 
-# It uses the simulation list to decide which "emulation" of the PPTM to use.
-# It can't actually "pick" non-deterministically, so it ultimately has to resort
-# To deterministically choosing a particular set of choices the PPTM might make.
-# However, because the PPTM specifically countermands it, this strategy can never succeed.
+# The pathological function.
+# Utilizes the list of choices to decide its behavior.
 def pathological_decider():
-    choices = pathological_decider.choices  # Set externally before calling
+    choices = pathological_decider.choices
     target = choices[-1]
     
     if target == 'pathological_decider_1':
@@ -61,8 +66,7 @@ def pathological_decider():
     elif target == 'pathological_decider_2':
         return "Doesn't halt"
 
-# Here, we test the PPTM against the range of pathological configurations.
-# We keep count of how many times the PPTM correctly determines the pathological's decision.
+# Testing the PPTM against various pathological configurations.
 max_length = 5
 correct = 0
 total = 0
