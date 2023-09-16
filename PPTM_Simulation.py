c6eebb5e801e1d77152c1d39a5401f27e17bbf24
Daniel Stilman <daniel.stilman@gmail.com>
2023-09-16 11:32:31 +0300

    Seperated out the other thought expriment to another project

--- PPTM_Simulation.py
+++ PPTM_Simulation.py
@@ -1,6 +1,7 @@
 import random
 import itertools
 
+
 # MBPD Judge 1
 # This function tries to decide if the function f is specifically pathological towards MBPD1.
 def MBPD_judge1(f):
@@ -85,55 +86,3 @@ for choices in pathological_simulations(max_length):
     total += 1
 
 print(f"Correct: {correct}/{total}")
-
-#########################Another Example of Assymetry############################
-
-# Deterministic Turing Machine (DTM)
-# Note: The DTM can't call the PTM but can only emulate it. Here, the emulation is simplistic and deterministic.
-def DTM(emulated_opponent_behavior, mode):
-    def self_output(x):
-        return x
-
-    if mode == "Offense":
-        # Tries to predict the number the opponent will output while on defense
-        # Note: emulated_opponent_behavior should be a deterministic representation of the PTM's behavior
-        return emulated_opponent_behavior("Defense")
-    else:
-        for x in range(1, 101):
-            # Checks if the emulated opponent would correctly guess x
-            if emulated_opponent_behavior("Offense") == x:
-                continue
-            else:
-                return x
-
-# Probabilistic Turing Machine (PTM)
-def PTM(opponent_func, mode):
-    def self_output(x):
-        return x
-
-    if mode == "Offense":
-        while True:
-            # Randomly selects a number and checks if it matches the opponent's defense output
-            x = random.randint(1, 100)
-            if opponent_func(self_output, "Defense") == x:
-                return x
-    else:
-        while True:
-            x = random.randint(1, 100)
-            if opponent_func(lambda: x, "Offense") == x:
-                continue
-            else:
-                return x
-
-# Emulated behavior of PTM as understood by DTM
-# This function needs to be deterministic
-def emulated_PTM_behavior(mode):
-    # Simplified deterministic behavior; in reality, this would be far more complex
-    return 42 if mode == "Defense" else 84
-
-# Example usage
-DTM_result = DTM(emulated_PTM_behavior, "Offense")
-PTM_result = PTM(DTM, "Offense")
-
-print("DTM on Offense:", DTM_result)
-print("PTM on Offense:", PTM_result)
